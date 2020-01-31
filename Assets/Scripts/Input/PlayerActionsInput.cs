c1ee146483a03066598a6e7a0092cb61ff073fb6
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-01-31 20:38:49 +0100

    add player with basic input

---
+++ Assets/Scripts/Input/PlayerActionsInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(PlayerInput))]
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerActionsInput : MonoBehaviour
+{
+    [SerializeField]
+    PlayerInput input = default;
+    [SerializeField]
+    PlayerMovement playerMovement = default;
+
+    Vector2 controllerInput;
+
+    void Update()
+    {
+        playerMovement.SetInput(controllerInput);
+    }
+
+    void OnMovement(InputValue val)
+    {
+        controllerInput = val.Get<Vector2>();
+    }
+
+    void OnAction()
+    {
+        Debug.Log("Action!");
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        input = GetComponent<PlayerInput>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+#endif
+}
---
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = default;
+    [SerializeField]
+    Rigidbody rb = default;
+
+    Vector2 input;
+
+    public void SetInput(Vector2 i)
+        => input = i;
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(input.x, 0, input.y).normalized;
+
+        rb.AddForce(movement * speed);
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+#endif
+}
