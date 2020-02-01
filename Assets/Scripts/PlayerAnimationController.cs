a47ba5c8405694b2bb478f3a16da4c892929f706
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-02-01 19:02:38 +0100

    add player mesh to game

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerAnimationController : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    [SerializeField]
+    Animator animator;
+
+    int velocityId;
+
+    void Start()
+    {
+        velocityId = Animator.StringToHash("Velocity");
+    }
+
+    void Update()
+    {
+        animator.SetFloat(velocityId, rb.velocity.magnitude);
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+#endif
+}
