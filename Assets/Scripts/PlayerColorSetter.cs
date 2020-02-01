62551e024508f1f24919038353c53c54ca32507f
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-02-01 20:43:04 +0100

    add players color changing

---
+++ Assets/Scripts/PlayerColorSetter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(PlayerInfo))]
+public class PlayerColorSetter : MonoBehaviour
+{
+    [System.Serializable]
+    struct MaterialPropertyInfo
+    {
+        public Renderer renderer;
+        public int index;
+    }
+
+    readonly int PLAYER_COLOR_ID = Shader.PropertyToID("_BaseColor");
+
+    [SerializeField]
+    MaterialPropertyInfo[] meshRenderers;
+
+    [SerializeField]
+    PlayerInfo playerInfo;
+
+
+    void Start()
+    {
+        var color = playerInfo.Color;
+
+
+        foreach (var r in meshRenderers)
+        {
+            var mp = new MaterialPropertyBlock();
+            r.renderer.GetPropertyBlock(mp, r.index);
+
+            mp.SetColor(PLAYER_COLOR_ID, color);
+
+            r.renderer.SetPropertyBlock(mp, r.index);
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        playerInfo = GetComponent<PlayerInfo>();
+    }
+#endif
+}
