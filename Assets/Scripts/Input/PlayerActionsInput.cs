9f2eadcf2fed6e93dc9820ee043048dced441450
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-02-02 12:51:34 +0100

    attempt to fix player input

--- Assets/Scripts/Input/PlayerActionsInput.cs
+++ Assets/Scripts/Input/PlayerActionsInput.cs
@@ -8,23 +8,25 @@ public class PlayerActionsInput : MonoBehaviour
 {
     [SerializeField]
     PlayerMovement playerMovement = default;
+    [SerializeField]
+    PlayerInput playerInput = default;
 
     Vector2 controllerInput;
 
-    void Update()
-    {
-        playerMovement.SetInput(controllerInput);
-    }
 
     void OnMovement(InputValue val)
     {
         controllerInput = val.Get<Vector2>();
+
+
+        playerMovement.SetInput(controllerInput);
     }
     
 #if UNITY_EDITOR
     void OnValidate()
     {
         playerMovement = GetComponent<PlayerMovement>();
+        playerInput = GetComponent<PlayerInput>();
     }
 #endif
 }
